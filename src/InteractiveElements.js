38faf4d7d9513692e7baf92174bee6c1b641aa90
Usman Suleiman <usmansbk@gmail.com>
2020-12-22 11:32:15 +0100

    refactor: Add functionality

--- src/InteractiveElements.js
+++ src/InteractiveElements.js
@@ -4,10 +4,14 @@ import {
   Text,
   TextInput,
   TouchableWithoutFeedback,
-  TouchableOpacity,
+  TouchableHighlight,
   View,
 } from 'react-native';
 import moment from 'moment';
+
+const INPUT_HEIGHT = 50;
+const BALL_HEIGHT = 32;
+const SMALL_BALL_HEIGHT = 20;
 
 const colors = [
   '46, 204, 112',
@@ -26,8 +30,14 @@ const getColor = (index = 0, opacity = 1) =>
 
 export default function InteractiveElements() {
   const [name, setName] = useState("Trader Joe's");
-  const [date] = useState(moment());
-  const [colorIndex] = useState(0);
+  const [date, setDate] = useState(moment());
+  const [colorIndex, setColorIndex] = useState(0);
+
+  const onChangeName = (value) => setName(value);
+  const onChangeColor = (newIndex) => setColorIndex(newIndex);
+  const setToday = () => setDate(moment());
+  const setTomorrow = () => setDate(moment().add(1, 'day'));
+  const nextDay = () => setDate(moment(date).add(1, 'day'));
 
   return (
     <View style={styles.container}>
@@ -36,16 +46,50 @@ export default function InteractiveElements() {
           <Text style={styles.label}>Event Name</Text>
           <TextInput
             value={name}
-            onChangeText={(text) => setName(text)}
+            onChangeText={onChangeName}
             style={styles.textInput}
           />
         </View>
 
+        <View style={styles.row}>
+          {colors.map((_, index) => {
+            return (
+              <TouchableWithoutFeedback
+                key={index}
+                onPress={() => onChangeColor(index)}>
+                <View style={styles.ballContainer}>
+                  <View
+                    style={[
+                      styles.ball,
+                      {
+                        backgroundColor: getColor(index),
+                      },
+                    ]}
+                  />
+                  {index === colorIndex && <View style={styles.indicator} />}
+                </View>
+              </TouchableWithoutFeedback>
+            );
+          })}
+        </View>
+
         <View>
           <Text style={styles.label}>Start Date</Text>
-          <View style={[styles.picker, styles.pickerOuterBorder]}>
+          <View
+            style={[
+              styles.picker,
+              {
+                backgroundColor: getColor(colorIndex, 0.8),
+              },
+            ]}>
             <TouchableWithoutFeedback>
-              <View style={[styles.pickerContent]}>
+              <View
+                style={[
+                  styles.pickerContent,
+                  {
+                    borderColor: getColor(colorIndex),
+                  },
+                ]}>
                 <Text style={styles.pickerText}>
                   {date.format('MMMM DD, YYYY')}
                 </Text>
@@ -55,20 +99,35 @@ export default function InteractiveElements() {
         </View>
 
         <View style={styles.buttons}>
-          <TouchableWithoutFeedback>
-            <Text style={[styles.button, styles.buttonText]}>Today</Text>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback>
-            <Text style={[styles.button, styles.buttonText]}>Tomorrow</Text>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback>
-            <Text style={[styles.button, styles.buttonText]}>+1</Text>
-          </TouchableWithoutFeedback>
+          <Button
+            text="Today"
+            onPress={setToday}
+            underlayColor={getColor(colorIndex, 0.4)}
+          />
+          <Button
+            text="Tomorrow"
+            onPress={setTomorrow}
+            underlayColor={getColor(colorIndex, 0.4)}
+          />
+          <Button
+            text="+1"
+            onPress={nextDay}
+            underlayColor={getColor(colorIndex, 0.4)}
+          />
         </View>
       </View>
     </View>
   );
 }
+
+const Button = ({text, onPress, underlayColor}) => (
+  <TouchableHighlight
+    underlayColor={underlayColor}
+    style={styles.button}
+    onPress={onPress}>
+    <Text style={[styles.buttonText]}>{text}</Text>
+  </TouchableHighlight>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -90,11 +149,10 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginVertical: 8,
     paddingHorizontal: 14,
-    height: 50,
+    height: INPUT_HEIGHT,
   },
   picker: {
-    height: 52,
-    backgroundColor: getColor(6, 0.8),
+    height: INPUT_HEIGHT + 2, // 2 is padding
     borderRadius: 8,
     marginVertical: 8,
     padding: 2,
@@ -106,7 +164,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
     borderRadius: 6,
     borderWidth: 1,
-    borderColor: getColor(6),
     alignItems: 'center',
   },
   pickerText: {
@@ -127,5 +184,26 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: 'gray',
     fontWeight: 'bold',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 16,
+  },
+  ball: {
+    width: BALL_HEIGHT,
+    height: BALL_HEIGHT,
+    borderRadius: BALL_HEIGHT / 2,
+  },
+  ballContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  indicator: {
+    position: 'absolute',
+    width: SMALL_BALL_HEIGHT,
+    height: SMALL_BALL_HEIGHT,
+    borderRadius: SMALL_BALL_HEIGHT / 2,
+    backgroundColor: 'white',
   },
 });
