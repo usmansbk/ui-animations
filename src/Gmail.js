d3997216b8cfd7d3848d95316f496ce374f3ab76
Usman Suleiman <usmansbk@gmail.com>
2020-12-24 16:22:19 +0100

    create components

--- src/Gmail.js
+++ src/Gmail.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {FlatList, StyleSheet, View, Text} from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+} from 'react-native';
+import moment from 'moment';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import data from '../assets/email';
 
@@ -20,26 +27,53 @@ export default function Gmail() {
   );
 }
 
-const Item = ({name, title, message}) => {
+const Item = ({name, date, title, message}) => {
   return (
-    <View style={styles.itemContainer}>
-      <View></View>
-      <View style={styles.itemContent}>
-        <Text style={styles.title} numberOfLines={1}>
-          {name}
-        </Text>
-        <View style={styles.itemSubtitle}>
-          <View style={styles.subtitleContainer}>
-            <Text style={styles.subtitle} numberOfLines={1}>
-              {title}
+    <TouchableHighlight
+      underlayColor={colors.gray}
+      onPress={() => null}
+      style={styles.itemContainer}>
+      <View style={styles.itemRow}>
+        <View>
+          <Avatar name={name} />
+        </View>
+        <View style={styles.itemContent}>
+          <View style={styles.itemHeader}>
+            <Text style={styles.title} numberOfLines={1}>
+              {name}
             </Text>
-            <Text style={styles.subtitle} numberOfLines={1}>
-              {message}
+            <Text style={styles.date}>
+              {moment(date, 'MM/dd/YYYY').format('D MMM')}
             </Text>
           </View>
-          <Icon name="star-outline" size={24} color={colors.gray2} />
+          <View style={styles.itemSubtitle}>
+            <View style={styles.subtitleContainer}>
+              <Text style={styles.subtitle} numberOfLines={1}>
+                {title}
+              </Text>
+              <Text style={styles.subtitle} numberOfLines={1}>
+                {message}
+              </Text>
+            </View>
+            <Icon name="star-outline" size={24} color={colors.gray2} />
+          </View>
         </View>
       </View>
+    </TouchableHighlight>
+  );
+};
+
+const Avatar = ({name}) => {
+  return (
+    <View
+      style={[
+        styles.avatar,
+
+        {
+          backgroundColor: getColor(name),
+        },
+      ]}>
+      <Text style={styles.initials}>{name[0]}</Text>
     </View>
   );
 };
@@ -63,6 +97,19 @@ const colors = {
   text: '#5d5d5d',
   gray2: '#a2a2a2',
 };
+
+const avatarColors = [
+  '#bdc3c7',
+  '#9b59b6',
+  '#f39c12',
+  '#1abc9c',
+  '#2ecc71',
+  '#3498db',
+];
+
+const getColor = (name = '') => avatarColors[name.length % avatarColors.length];
+
+const AVATAR_SIZE = 40;
 
 const styles = StyleSheet.create({
   container: {
@@ -89,27 +136,56 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 16,
     color: colors.text,
+    flex: 1,
   },
   subtitle: {
     color: colors.text,
   },
   itemContainer: {
-    paddingHorizontal: 12,
-    paddingVertical: 12,
+    paddingRight: 12,
+    paddingVertical: 16,
     marginVertical: 2,
-    marginHorizontal: 8,
+    marginLeft: 4,
     borderRadius: 16,
+    borderBottomRightRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  itemRow: {
+    flexDirection: 'row',
   },
   itemContent: {
     flex: 1,
+    paddingLeft: 4,
   },
   itemSubtitle: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'flex-end',
   },
+  itemHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
   subtitleContainer: {
     flex: 1,
     paddingRight: 8,
+  },
+  date: {
+    fontSize: 12,
+    color: colors.text,
+  },
+  avatar: {
+    width: AVATAR_SIZE,
+    height: AVATAR_SIZE,
+    borderRadius: AVATAR_SIZE / 2,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 12,
+    marginVertical: 2,
+  },
+  initials: {
+    color: 'white',
+    fontSize: AVATAR_SIZE / 2,
   },
 });
