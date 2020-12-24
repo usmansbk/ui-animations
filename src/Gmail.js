f6a40b6ba8a9c1995971c27a07dbeeff53a883b5
Usman Suleiman <usmansbk@gmail.com>
2020-12-24 07:36:38 +0100

    chore

---
+++ src/Gmail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+export default function Gmail() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
