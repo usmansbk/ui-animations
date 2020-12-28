27419153c4f374bdcb5bebf16947dba32913af42
Usman Suleiman <usmansbk@gmail.com>
2020-12-28 11:25:55 +0100

    chore

--- src/DraggableFlatList.js
+++ src/DraggableFlatList.js
@@ -1,28 +1,57 @@
 import React from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Animated} from 'react-native';
 
-const data = Array.from(new Array(50).fill(0).map((_, index) => index + ''));
+const colors = [
+  '#3498db',
+  '#1abc9c',
+  '#2ecc71',
+  '#3498db',
+  '#34495e',
+  '#16a085',
+  '#27ae60',
+  '#2980b9',
+  '#8e44ad',
+  '#f1c40f',
+  '#e67e22',
+  '#e74c3c',
+  '#f39c12',
+  '#d35400',
+];
+const MIN_HEIGHT = 40;
+const MAX_HEIGHT = 90;
+const DIFF_HEIGHT = MAX_HEIGHT - MIN_HEIGHT;
+const getColor = (index) => colors[index % colors.length];
+const getRandomHeight = () => Math.random() * DIFF_HEIGHT + MIN_HEIGHT;
+
+const data = Array.from(
+  new Array(50).fill(0).map((_, index) => ({
+    id: index + '',
+    text: index,
+    color: getColor(index),
+    height: getRandomHeight(),
+  })),
+);
 
 export default function DraggableFlatList() {
-  const renderItem = ({item, index}) => {
+  const renderItem = ({item}) => {
     return (
       <View
         style={[
           styles.itemContainer,
-          {backgroundColor: getColor(index), height: getRandomHeight()},
+          {backgroundColor: item.color, height: item.height},
         ]}>
-        <Text style={styles.text}>{item}</Text>
+        <Text style={styles.text}>{item.text}</Text>
       </View>
     );
   };
   return (
-    <View style={styles.container}>
-      <FlatList
+    <Animated.View style={styles.container}>
+      <Animated.FlatList
         data={data}
-        keyExtractor={(item) => item}
+        keyExtractor={(item) => item.id}
         renderItem={renderItem}
       />
-    </View>
+    </Animated.View>
   );
 }
 
@@ -42,26 +71,3 @@ const styles = StyleSheet.create({
     color: 'white',
   },
 });
-
-const MIN_HEIGHT = 40;
-const MAX_HEIGHT = 90;
-const DIFF_HEIGHT = MAX_HEIGHT - MIN_HEIGHT;
-const getColor = (index) => colors[index % colors.length];
-const getRandomHeight = () => Math.random() * DIFF_HEIGHT + MIN_HEIGHT;
-
-const colors = [
-  '#3498db',
-  '#1abc9c',
-  '#2ecc71',
-  '#3498db',
-  '#34495e',
-  '#16a085',
-  '#27ae60',
-  '#2980b9',
-  '#8e44ad',
-  '#f1c40f',
-  '#e67e22',
-  '#e74c3c',
-  '#f39c12',
-  '#d35400',
-];
